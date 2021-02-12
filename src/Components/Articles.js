dd158c5a930c88b5fb620bf22dc206f8071004c5
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2021-02-12 19:28:47 -0300

    ortografia corregida

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -10,17 +10,17 @@ const Articles = () => {
             <div className="article-container text-center">
                 <div className="col-sm-12 col-md-12 col-lg-4">
                 <img src={chatbot} alt="img chatbot" width="40%"></img>
-                <h3><strong>Chatbot <br></br>personalizado</strong></h3>
-                <p style={{fontSize: "17px", color: "#9a9a9a" }} className="mt-3">Daremos vida a tus redes sociales,<br></br>planificaremos una conversación <br></br>exitosa con tu cliente, dando la mayor <br></br>información sobre tu negocio.</p>
+                <h3 style={{textTransform:"initial", fontSize:"25px", letterSpacing:"0em"}} className="mt-2"><strong>Chatbot <br></br>personalizado</strong></h3>
+                <p style={{fontSize: "17px", color: "#9a9a9a" }} className="mt-4">Daremos vida a tus redes sociales,<br></br>planificaremos una conversación <br></br>exitosa con tu cliente, dando la mayor <br></br>información sobre tu negocio.</p>
                 </div>
                 <div className="col-sm-12 col-md-12 col-lg-4">
                     <img src={branding} alt="img branding" width="40%"></img>
-                    <h3 className="mt-3"><strong>Branding y diseño <br></br>de marca</strong></h3>
+                    <h3 style={{textTransform:"initial", fontSize:"25px", letterSpacing:"0em" }} className="mt-3"><strong>Branding y diseño <br></br>de marca</strong></h3>
                     <p style={{fontSize: "17px", color: "#9a9a9a" }} className="mt-3">Construiremos o renovaremos tu marca <br></br> de manera profesional, coherente y <br></br> adecuado a los últimos estándares.</p>
                 </div>
                 <div className="col-sm-12 col-md-12 col-lg-4">
                     <img src={web} alt="img web" width="40%"></img>
-                <h3><strong>Desarrollo<br></br>web</strong></h3>
+                <h3 style={{textTransform:"initial", fontSize:"25px", letterSpacing:"0em"}} className="mt-2"><strong>Desarrollo<br></br>web</strong></h3>
                 <p style={{fontSize: "17px", color: "#9a9a9a" }} className="mt-3">Desarrollaremos la presencia digital <br></br>de tu negocio con una página web <br></br>optimizada para atraer consultas.</p>
                 </div>
             </div>
