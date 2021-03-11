90c0dc8a27c6a5dd977248c3a754a0bd631490e3
alejandro lazarte <alejandrolazarte8@gmail.com>
2021-03-11 20:44:18 -0300

    fix

--- src/Otros/Carrusel.js
+++ src/Otros/Carrusel.js
@@ -7,20 +7,20 @@ import Video from '../videos/Video de Alejandro.mp4'
 const Nosotros = () => {
 
     return (
+        <div id="section1" className='player-wrapper'>
 
-
-                <div  id="section1" className='player-wrapper'>
-                  
-                    <ReactPlayer
-                        className='react-player'
-                        width="100%"
-                        height="100%"
-                        playing
-                        allow='autoplay; encrypted-media'
-                        url={Video}
-                    />
-                </div>
-
+            <ReactPlayer
+                className='react-player'
+                width="100%"
+                height="100%"
+                controls={true}
+                playing={true}
+                loop
+                muted
+                allow='autoplay; encrypted-media'
+                url={Video}
+            />
+        </div>
     );
 };
 
