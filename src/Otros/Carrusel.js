37cae68ccf27ada7f2b2e408055bb3db23f04f55
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2021-03-16 21:02:02 -0300

    mejor

--- src/Otros/Carrusel.js
+++ src/Otros/Carrusel.js
@@ -8,10 +8,10 @@ const Nosotros = () => {
 
   return (
     <div className="container-carrusel">
-      <div className="carrusel-img col-md-6">
+      <div className="carrusel-img col-md-6 pr-0">
         <img src={slider1} alt="slider" className="img-slider"></img>
       </div>
-      <div className="col-sm-12 col-md-6">
+      <div className="col-sm-12 col-md-6 pl-0">
         <img src={imgCapez} alt="capez" className="img-capez"></img>
       </div>
     </div>
